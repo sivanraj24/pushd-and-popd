99b55b8808a390648d81a711fa3266c937d62d3c
sivanraj24 <113350376+sivanraj24@users.noreply.github.com>
2024-07-10 14:20:00 +0530

    Update pushd_popd.c

--- pushd_popd.c
+++ pushd_popd.c
@@ -3,93 +3,73 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
+#define STACK_SIZE 10
 
-typedef struct Node {
-    char dir[PATH_MAX];
-    struct Node* next;
-} Node;
-
-typedef struct Stack {
-    Node* top;
+typedef struct {
+    char dirs[STACK_SIZE][PATH_MAX];
+    int top;
 } Stack;
 
 void initStack(Stack* stack) {
-    stack->top = NULL;
+    stack->top = -1;
 }
 
 void pushd(Stack* stack, const char* dir) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(EXIT_FAILURE);
+    if (stack->top >= STACK_SIZE - 1) {
+        fprintf(stderr, "Stack overflow\n");
+        return;
     }
-    strncpy(newNode->dir, dir, PATH_MAX);
-    newNode->next = stack->top;
-    stack->top = newNode;
+    stack->top++;
+    strncpy(stack->dirs[stack->top], dir, PATH_MAX);
 }
 
 char* popd(Stack* stack) {
-    if (stack->top == NULL) {
-        fprintf(stderr, "Stack is empty\n");
+    if (stack->top < 0) {
+        fprintf(stderr, "Stack underflow\n");
         return NULL;
     }
-    Node* temp = stack->top;
-    stack->top = stack->top->next;
-    char* dir = strdup(temp->dir);
-    free(temp);
-    return dir;
-}
-
-
-char* getcwd_str() {
-    char* buffer = (char*)malloc(PATH_MAX);
-    if (buffer == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    if (getcwd(buffer, PATH_MAX) == NULL) {
-        perror("getcwd");
-        free(buffer);
-        exit(EXIT_FAILURE);
-}
-    return buffer;
+    return stack->dirs[stack->top--];
 }
 
 int main() {
     Stack dirStack;
     initStack(&dirStack);
-    
-    char* currentDir = getcwd_str();
-    printf("Current Directory: %s\n", currentDir);
 
+    char currentDir[PATH_MAX];
+    if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+        perror("getcwd");
+        return 1;
+    }
+    printf("Current Directory: %s\n", currentDir);
+    
     pushd(&dirStack, currentDir);
-    free(currentDir);
 
     if (chdir("/tmp") == 0) {
-        currentDir = getcwd_str();
+        if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+            perror("getcwd");
+            return 1;
+        }
         printf("Changed to Directory: %s\n", currentDir);
 
-       
         pushd(&dirStack, currentDir);
-        free(currentDir);
     } else {
         perror("chdir");
     }
+    
     char* previousDir = popd(&dirStack);
     if (previousDir != NULL) {
         printf("Popped Directory: %s\n", previousDir);
         if (chdir(previousDir) == 0) {
-            currentDir = getcwd_str();
+            if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+                perror("getcwd");
+                return 1;
+            }
             printf("Changed back to Directory: %s\n", currentDir);
-            free(currentDir);
         } else {
             perror("chdir");
         }
-        free(previousDir);
-    }
-    while ((previousDir = popd(&dirStack)) != NULL) {
-        free(previousDir);
     }
 
     return 0;
 }
+
